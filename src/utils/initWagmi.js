9fa9681611703a879c725a7081f2fa3189367025
tschernysh <tschernysh@gmail.com>
2023-09-22 01:47:56 +0300

    added connect wallet with polygon

--- src/utils/initWagmi.js
+++ src/utils/initWagmi.js
@@ -1,10 +1,10 @@
 import { EthereumClient, w3mConnectors, w3mProvider } from '@web3modal/ethereum'
 import { Config } from 'config'
 import { configureChains, createConfig } from 'wagmi'
-import { bsc, bscTestnet } from 'wagmi/chains'
+import { bsc, bscTestnet, polygon, polygonMumbai } from 'wagmi/chains'
 
 export const initWagmi = () => {
-  const chains = [bsc, bscTestnet]
+  const chains = [polygon, polygonMumbai]
   const projectId = Config().PROJECT_ID
 
   const { publicClient } = configureChains(chains, [w3mProvider({ projectId })])
--- tailwind.config.js
+++ tailwind.config.js
@@ -9,46 +9,44 @@ module.exports = {
         'c': '6.25rem',
       },
       colors: {
-        'white': '#ECE8DE',
-        'gold': '#E2B20B',
-        'title': '#ECE8DE',
-        'description': '#B0ACA2',
-        'blue': '#0A2C61',
-        'prussian': '#1C2E3F',
-        'darkBlue': '#020123',
-        'blueDisabled': '#383451',
-        'black': '#1B1A18'
+        white: '#ECE8DE',
+        gold: '#E2B20B',
+        title: '#ECE8DE',
+        description: '#B0ACA2',
+        blue: '#0A2C61',
+        prussian: '#1C2E3F',
+        darkBlue: '#020123',
+        blueDisabled: '#383451',
+        black: '#1B1A18'
       },
       textColors: {
-        'hite': '#ECE8DE',
-        'gold': '#E2B20B',
-        'title': '#ECE8DE',
-        'description': '#B0ACA2',
-        'blue': '#0A2C61',
-        'prussian': '#1C2E3F',
-        'darkBlue': '#020123',
-        'blueDisabled': '#383451',
-        'black': '#1B1A18'
+        white: '#ECE8DE',
+        gold: '#E2B20B',
+        title: '#ECE8DE',
+        description: '#B0ACA2',
+        blue: '#0A2C61',
+        prussian: '#1C2E3F',
+        darkBlue: '#020123',
+        blueDisabled: '#383451',
+        black: '#1B1A18'
+      },
+      screens: {
+        sm: '640px',
+        md: '768px',
+        lg: '1024px',
+        xl: '1140px',
+        xxl: '1200px',
+        mxx: '1350px',
+        mmx: '1500px'
+      },
+      fontFamily: {
+        inter400: ['Inter-Regular', 'sans-serif'],
+        inter600: ['Inter-SemiBold', 'sans-serif'],
+        inter800: ['Inter-ExtraBold', 'sans-serif'],
+        poppins400: ['Poppins-Regular', 'sans-serif'],
+        poppins600: ['Poppins-SemiBold', 'sans-serif'],
       }
-
     },
-    screens: {
-      sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1140px',
-      xxl: '1200px',
-      mxx: '1350px',
-      mmx: '1500px'
-    },
-    fontFamily: {
-      inter400: ['Inter-Regular', 'sans-serif'],
-      inter600: ['Inter-SemiBold', 'sans-serif'],
-      inter800: ['Inter-ExtraBold', 'sans-serif'],
-      poppins400: ['Poppins-Regular', 'sans-serif'],
-      poppins600: ['Poppins-SemiBold', 'sans-serif'],
-    }
   },
   plugins: [],
 }
-
