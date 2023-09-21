d37c20c54ba29aeefa5ea3ee9982be685cd56bd6
tschernysh <tschernysh@gmail.com>
2023-09-22 01:00:50 +0300

    pre craco app

--- src/App.js
+++ src/App.js
@@ -4,14 +4,101 @@ import { BrowserRouter } from 'react-router-dom';
 import { Header } from 'Components/Header/Header';
 import { Footer } from 'Components/Footer/Footer';
 import { ApplicationRoutes } from 'Routes/ApplicationRoutes';
+import { useDispatch, useSelector } from 'react-redux';
+import { useEffect, useMemo } from 'react';
+import { initWagmi } from 'utils/initWagmi';
+import { WagmiConfig } from 'wagmi';
+import { ApplicationActionCreator } from 'store/reducers/application/action-creator';
 
 function App() {
+  const dispatch = useDispatch()
+  const {
+    walletAddress, isNeedUpdate, notCorrectChain
+  } = useSelector(state => state.ApplicationReducer)
+  const [seconds, setSeconds] = useState(0)
+  const [wagmiConfig, setWagmiConfig] = useState()
+  const [ethereumClient, setEthereumClient] = useState()
+  const [projectId, setProjectId] = useState()
+
+  useMemo(() => {
+    const { wagmiConfig, ethereumClient, projectId } = initWagmi()
+
+    setWagmiConfig(wagmiConfig)
+    setEthereumClient(ethereumClient)
+    setProjectId(projectId)
+  }, [])
+
+  useEffect(() => {
+    let interval
+    interval = setInterval(() => {
+      if (!seconds) {
+        if (notCorrectChain) return
+
+        if (!!walletAddress) {
+        }
+        setSeconds(1);
+      } else if (seconds > config().HEARTBEAT_RATE) {
+        setSeconds(0)
+      } else setSeconds(seconds + 1)
+    }, 1000)
+
+    return () => clearInterval(interval);
+  }, [seconds])
+
+  useEffect(() => {
+    if (isNeedUpdate) {
+      if (!!walletAddress) {
+      }
+      dispatch(ApplicationActionCreator.setIsNeedUpdate(false))
+    }
+  }, [isNeedUpdate])
+
+  useEffect(() => {
+    if (notCorrectChain) alert('Connect to another supported chain')
+    if (!!walletAddress && !notCorrectChain) {
+      setSeconds(0)
+    }
+  }, [walletAddress, notCorrectChain])
+
+  useEffect(() => {
+    // Function to handle wallet change event
+    const handleAccountsChanged = (accounts) => {
+      dispatch(ApplicationActionCreator.setWalletAddress(accounts[0]))
+      console.log('Active wallet changed:', accounts);
+    };
+
+    const handleChainChanged = (chainId) => {
+      const newChainId = Number(chainId)
+
+      if (chainId !== config().CHAIN_ID) {
+        dispatch(ApplicationActionCreator.setNotCorrectChain(false))
+      } else dispatch(ApplicationActionCreator.setNotCorrectChain(true))
+    }
+
+    // Check if MetaMask is available
+    if (typeof window.ethereum !== 'undefined') {
+      // Subscribe to wallet change events
+      window.ethereum.on('accountsChanged', handleAccountsChanged);
+      window.ethereum.on('chainChanged', handleChainChanged);
+    }
+
+    // Clean up the subscription on component unmount
+    return () => {
+      if (typeof window.ethereum !== 'undefined') {
+        window.ethereum.removeListener('accountsChanged', handleAccountsChanged);
+        window.ethereum.removeListener('chainChanged', handleChainChanged);
+      }
+    };
+  }, []);
+
+
   return (
     <BrowserRouter>
-      <Header />
-      <ApplicationRoutes />
-      <Footer />
-
+      <WagmiConfig config={wagmiConfig}>
+        <Header />
+        <ApplicationRoutes projectId={projectId} ethereumClient={ethereumClient} />
+        <Footer />
+      </WagmiConfig>
     </BrowserRouter>
   );
 }
--- src/Routes/ApplicationRoutes.jsx
+++ src/Routes/ApplicationRoutes.jsx
@@ -3,12 +3,47 @@ import { MainPage } from "Pages/MainPage/MainPage"
 import { WorkPage } from "Pages/WorkPage/WorkPage"
 import { ReferralPage } from "Pages/ReferralPage/ReferralPage"
 import { ResultsPage } from "Pages/ResultsPage/ResultsPage"
+import { useAccount, useWalletClient } from "wagmi"
+import { useDispatch } from "react-redux"
+import { AccountActionCreator } from "store/reducers/account/action-creator"
+import { ApplicationActionCreator } from "store/reducers/application/action-creator"
+import { useEffect } from "react"
+import { Web3Modal } from "@web3modal/react"
 
 
 export const ApplicationRoutes = () => {
 
+  const { connector: activeConnector, address, isConnecting, isDisconnected } = useAccount()
+  const { data, isError } = useWalletClient()
+  const dispatch = useDispatch()
+
+  const disconnectWallet = () => {
+    dispatch(AccountActionCreator.resetUserInfo())
+    dispatch(ApplicationActionCreator.setWalletAddress(null))
+  }
+
+  useEffect(() => {
+    console.log(address, data)
+    if (!!data && !isDisconnected) {
+      dispatch(ApplicationActionCreator.setWeb3(data))
+      dispatch(ApplicationActionCreator.connectConnectWallet())
+    } else if (data === undefined && isDisconnected === undefined) {
+      disconnectWallet()
+    }
+  }, [address, data])
+
+  useEffect(() => {
+    if (isDisconnected) {
+      disconnectWallet()
+    }
+  }, [isDisconnected])
+
   return (
     <>
+      <Web3Modal explorerRecommendedWalletIds={[
+        'c57ca95b47569778a828d19178114f4db188b89b763c899ba0be274e97267d96',
+        '4622a2b2d6af1c9844944291e5e7351a6aa24cd7b23099efac1b2fd875da31a0'
+      ]} projectId={props.projectId} ethereumClient={props.ethereumClient} />
       <Routes>
         <Route path='/' element={<MainPage />} />
         <Route path='/works' element={<WorkPage />} />
